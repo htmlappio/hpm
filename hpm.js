802358cd13b5c5dd470504206aff3b0a3d00fb19
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2015-12-23 21:33:39 +0100

    Working...

--- hpm.js
+++ hpm.js
@@ -2,6 +2,7 @@
 // Copyright Jonas Colmsjö 2015
 //
 
+
 ;
 (function () {
   var log = console.log.bind(console);
@@ -19,21 +20,36 @@
   // private vars
   // ===========
 
-  var schema = {
-    stores: [{
-      name: 'apps',
-      autoIncrement: false
-    }]
-  };
-
-  var db = new ydn.db.Storage('htmlapps', schema);
-
   // hpm Class
   // =========
 
   // constructor
   var hpm = {};
 
+  // NOTE: Should create a proper class, perhaps as a singleton???
+  hpm.getSetup = function() {
+    var db = new ydn.db.Storage('config', {
+      stores: [{ name: 'backend', autoIncrement: false }]
+    });
+
+    return db.get('backend', 'setup');
+  };
+
+  hpm.create = function(packageDef, html, css, js, workStore) {
+    var db = new ydn.db.Storage('config', {
+      stores: [{ name: 'backend', autoIncrement: false },
+               { name: workStore, autoIncrement: false }]
+    });
+
+    var d = {};
+    Promise.all([
+      {d.package_def = db.get(package_def_file);}
+      {d.html = db.get(html_file)}
+      {d.css = db.get(css_file) }
+      {d.js = db.get(js_file ) }
+    ]).then(func(res) {db.put(b_) })
+    
+  };
 
   // Command line help, static functions on the App object
   // -----------------------------------------------------
@@ -42,10 +58,64 @@
 
     if (!topic) {
       info('-- Htmlapp package manager help --');
+      info('* hpm.help("setup") - show setup help');
       info('* hpm.register(<package name>) - register a package in the remote registry.');
       info('* hpm.install(<package name>) - install a remote package locally.');
+
+      var msg =
+        'create(package_def_file, html_file, css_file, js_file, work_store) - overwrite existing, check version?' +
+        'load(name, [version]) - take latest version if not specified' +
+        'sync() - uppdat registry med public packages, varna om name är upptaget' +
+        'register(name) - spara rad i b_packages: <account_id>, app id' +
+        'fetch(name) - kolla att name finns, spara rad i b_packages, hämta vid nästa sync' +
+        'search(keywords) - lista packages som matchar, registry endast remote, ej lokalt?' ;
+
+      info(msg);
+
       return;
     }
+
+    if (topic === 'setup') {
+      var msg = 'A little configuration needs to be done before hpm can be used:' +
+        '\nvar setup = {' +
+        '\n\turl: "http://localhost:9000/", ' +
+        '\n\temail: "joe@example,com"' +
+        '\n};' +
+        '\nvar db = new ydn.db.Storage("config", {stores: [{ name: "backend", autoIncrement: false }]});' +
+        '\nOdata.createAccount(setup.url, setup.email).then(function(res){' +
+        '\n\tsetup.accountId = res.data.d.accountId; ' +
+        '\n\tdb.put("backend", setup, "setup");' +
+        '\n});'
+
+      info(msg);
+    }
+
+    else if (topic === 'create') {
+      var msg =
+        'Your packages are save in a database named with your accountid and the ' +
+        'store named bucket. The key for each packet is b_<package name>-<version>' +
+        '<version> should be in the format X.Y.Z, e.g. 1.0.0, using so called ' +
+        'semantic versions, see semver.org';
+
+      info(msg);
+
+      var msg =
+        '\nvar buckets = new ydn.db.Storage(accountId, {stores: [{ name: "buckets", autoIncrement: false }]});' +
+        '\ndb.put("buckets", { ' +
+        '\n\t   name:' +
+        '\n\t    description:' +
+        '\n\t    version:' +
+        '\n\t    private:' +
+        '\n\t    misc info fields...' +
+        '\n\t  }, "mypackage.json");' +
+        '\n{ package_def: {...},' +
+        '\n\t  html: "",' +
+        '\n\t  css: "",' +
+        '\n\t  js: "" }';
+
+      info(msg);
+    }
+
 
   }
 
